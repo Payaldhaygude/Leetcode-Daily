3f2c7540da831a982dec732d6145d9b5e50e34ba
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-03-29 23:28:19 +0530

    Update insert_at_position.cpp

--- insert_at_position.cpp
+++ insert_at_position.cpp
@@ -1,2 +1,10 @@
 // 🔗 Problem: https://leetcode.com/problems/search-insert-position/
 // 🟢 Difficulty: Easy
+
+// 💡 Approach:
+// Use Binary Search since the array is sorted.
+// If target is found → return index
+// Else return the position where it should be inserted
+
+// ⏱ Time Complexity: O(log n)
+// 🧠 Space Complexity: O(1)
