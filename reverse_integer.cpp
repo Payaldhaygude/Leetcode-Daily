09c71349ace5e1108ea6e43a880641a6229158db
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-03-30 20:55:14 +0530

    Update reverse_integer.cpp

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -20,9 +20,7 @@ public:
         while(x != 0) {
           digit = x % 10;
 
-          if (reverse_num > INT_MAX /10 || reverse_num < INT_MIN /10 ) {
-            return 0;
-          }
+          
           reverse_num = reverse_num * 10 + digit ;
         }
         return reverse_num;
