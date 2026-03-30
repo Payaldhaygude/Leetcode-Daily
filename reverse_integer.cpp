3496f775becb4c454912d28d5d0f213b9bdbe87a
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-03-30 20:42:31 +0530

    Create reverse_integer.cpp

---
+++ reverse_integer.cpp
@@ -0,0 +1,12 @@
+// 🔗 Problem: https://leetcode.com/problems/reverse-integer/
+// 🟢 Difficulty: Medium
+
+// 💡 Approach:
+// Extract digits one by one using modulo (% 10)
+// Build the reversed number using:
+// reverse = reverse * 10 + digit
+// Remove last digit using division (/ 10)
+// Handle overflow before updating reverse number
+
+// ⏱ Time Complexity: O(log n)
+// 🧠 Space Complexity: O(1)
