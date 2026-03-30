001473c7cec765d66b37e0c77de7933734e909bd
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-03-30 21:01:20 +0530

    Update reverse_integer.cpp

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -13,19 +13,19 @@
 
 class Solution {
 public:
-    int reverse(int x) {
+    int reverse(int n) {
         int digit;
         int reverse_num = 0;
 
-        while (x != 0) {
-            digit = x % 10;
+        while (n != 0) {
+            digit = n % 10;
 
             // Handle overflow
             if (reverse_num > INT_MAX / 10 || reverse_num < INT_MIN / 10)
                 return 0;
 
             reverse_num = reverse_num * 10 + digit;
-            x = x / 10;
+            n = n / 10;
         }
 
         return reverse_num;
