867577a16a2baafd1189b8bd01054249bf01927c
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-04-03 23:17:48 +0530

    Update MaxSum_array.cpp

--- MaxSum_array.cpp
+++ MaxSum_array.cpp
@@ -1,3 +1,7 @@
 // 🔗 Problem: https://leetcode.com/problems/maximum-subarray/
 // 🟢 Difficulty: Easy
-
+// 💡 Approach:
+// We use Kadane’s Algorithm.
+// - Keep a running sum of the current subarray.
+// - If the sum becomes negative, reset it to 0.
+// - At each step, update the maximum sum found so far.
