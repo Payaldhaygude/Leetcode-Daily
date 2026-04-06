eadb5311a4ef68bba351f97f19dbdc235c91f20b
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-04-06 23:08:55 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -9,3 +9,40 @@
 
 // ⏱ Time Complexity: O(n^2)
 // 🧠 Space Complexity: O(1) (excluding output)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> arr;
+        sort(nums.begin() , nums.end());
+
+        for(int i=0; i<nums.size(); i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    arr.push_back({nums[i] , nums[left] , nums[right]});  
+
+                    // skip duplicates
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+
+                    left++;
+                    right--;
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+        return arr;
+    }
+};
