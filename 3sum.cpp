26b13f16646ab49ef25501fc2df69ae146ee8301
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-04-06 23:07:19 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,3 +1,8 @@
 // 🔗 Problem: https://leetcode.com/problems/3sum/
 // 🟡 Difficulty: Medium
+// 💡 Approach:
+// 1. Sort the array.
+// 2. Fix one element (i).
+// 3. Use two pointers (left & right) to find pairs such that sum = 0.
+// 4. Skip duplicates to avoid repeated triplets.
 
