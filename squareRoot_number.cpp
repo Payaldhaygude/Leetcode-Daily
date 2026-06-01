4424df5f0b32ff3df82d53ccbfc3d7ea73737aaa
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-06-01 16:10:04 +0530

    Update squareRoot_number.cpp

--- squareRoot_number.cpp
+++ squareRoot_number.cpp
@@ -1,21 +1,23 @@
-// using math function for c++ -> sqrt
+// 🔗 Problem: https://leetcode.com/problems/sqrtx/
+// 🟢 Difficulty: Easy
 
+// 💡 Approach:
+// Use the built-in sqrt() function from the C++ Math Library (<cmath>)
+// to find the square root of x.
 
-
-
-
-
-
+// ⏱ Time Complexity: O(1)
+// 🧠 Space Complexity: O(1)
 
 class Solution {
 public:
     int mySqrt(int x) {
         int result = 0;
-        for(int i=1;i<sizeof(x);i++) {
+
+        for(int i = 1; i < sizeof(x); i++) {
             result = sqrt(x);
             cout << result;
         }
+
         return result;
-        
     }
 };
