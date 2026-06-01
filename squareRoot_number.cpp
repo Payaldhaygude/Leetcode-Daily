8030dbc6b273cd11a7c6ef4eb168e8b92102fbdd
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-06-01 16:02:34 +0530

    Create squareRoot_number.cpp

---
+++ squareRoot_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int result = 0;
+        for(int i=1;i<sizeof(x);i++) {
+            result = sqrt(x);
+            cout << result;
+        }
+        return result;
+        
+    }
+};
