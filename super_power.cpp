b997cc2aa6704fdc4f138f92d623efdcf8f2e32a
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-06-27 09:43:25 +0530

    Update super_power.cpp

--- super_power.cpp
+++ super_power.cpp
@@ -1,5 +1,3 @@
-
-
 // Approach:
 // If the exponent is represented as digits [d1, d2, ..., dk],
 // then:
@@ -13,3 +11,7 @@
 //
 // Time Complexity: O(n * log 10)
 // Space Complexity: O(n) due to recursion.
+
+part1 = (a^(remaining exponent))^10
+part2 = a^(last digit)
+answer = (part1 * part2) % 1337
