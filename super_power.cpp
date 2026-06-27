611116a99cd78df97e0f75dec5d010c544d8bd0f
Payal Dhaygude <payaldhaygude0366@gmail.com>
2026-06-27 09:42:35 +0530

    Create super_power.cpp

---
+++ super_power.cpp
@@ -0,0 +1,15 @@
+
+
+// Approach:
+// If the exponent is represented as digits [d1, d2, ..., dk],
+// then:
+//
+// a^(d1d2...dk)
+// = (a^(d1d2...d(k-1)))^10 * a^(dk)
+//
+// We recursively remove the last digit of the exponent.
+// modPow() is used to efficiently calculate powers modulo 1337
+// using Binary Exponentiation.
+//
+// Time Complexity: O(n * log 10)
+// Space Complexity: O(n) due to recursion.
